b914b9a550fec563ca89ca26da1bad098417e249
Felipe Buiras <felipe.buiras@maecenas.co>
2018-04-05 13:16:44 -0300

    Rename expres middlewares as such, add standalone logger and use it in request

--- teleferic/src/request.js
+++ teleferic/src/request.js
@@ -1,10 +1,12 @@
 const http = require('http')
 const querystring = require('querystring')
+const {logger} = require('./log')
 
 const AUTHORIZER_ENDPOINT_HOSTNAME = process.env.AUTHORIZER_ENDPOINT_HOSTNAME
 const AUTHORIZER_ENDPOINT_PORT = process.env.AUTHORIZER_ENDPOINT_PORT
 
 const request = (endpointName, data, httpMethod) => new Promise((resolve, reject) => {
+    logger.info(`Requesting data from ${endpointName}, request data is ${data}`)
     var path, headers
     data = data || {}
     if (httpMethod === 'GET' || !httpMethod)
@@ -15,6 +17,8 @@ const request = (endpointName, data, httpMethod) => new Promise((resolve, reject
             "Content-Type" : "application/json"
         }
     }
+    logger.info(`Request path is ${path}`)
+    logger.info(`Request headers are ${headers}`)
     let options = {
         host: AUTHORIZER_ENDPOINT_HOSTNAME,
         port: AUTHORIZER_ENDPOINT_PORT,
@@ -22,6 +26,7 @@ const request = (endpointName, data, httpMethod) => new Promise((resolve, reject
         method: httpMethod,
         headers: headers
     }
+    logger.info(JSON.stringify(options))
     let request = http.request(options, (response) => {
         let data = ""
         response.on('error', e => {
@@ -32,12 +37,21 @@ const request = (endpointName, data, httpMethod) => new Promise((resolve, reject
         })
         response.on('end', e => {
             if (response.statusCode >= 400)
-                reject(JSON.parse(data).error)
-            resolve(JSON.parse(data))
+                try {
+                    reject(JSON.parse(data).error)
+                } catch (e) {
+                    reject(data)
+                }
+            else {
+                resolve(JSON.parse(data))
+            }
         })
     })
-    if (httpMethod && httpMethod !== 'GET')
+    if (httpMethod && httpMethod !== 'GET'){
+        logger.info(`Request method is POST, writing data to request body.`)
+        logger.info(JSON.stringify(data))   
         request.write(JSON.stringify(data))
+    }
     request.end()
 
 })
