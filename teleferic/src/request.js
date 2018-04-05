82886b0cf92ed6db23c0ad7f0649ba88a120762d
Felipe Buiras <felipe.buiras@maecenas.co>
2018-04-05 11:56:36 -0300

    remove logging from request

--- teleferic/src/request.js
+++ teleferic/src/request.js
@@ -1,12 +1,10 @@
 const http = require('http')
 const querystring = require('querystring')
-const {logger} = require('winston')
 
 const AUTHORIZER_ENDPOINT_HOSTNAME = process.env.AUTHORIZER_ENDPOINT_HOSTNAME
 const AUTHORIZER_ENDPOINT_PORT = process.env.AUTHORIZER_ENDPOINT_PORT
 
 const request = (endpointName, data, httpMethod) => new Promise((resolve, reject) => {
-    logger.info(`Requesting data (${httpMethod}) from Authorizer on ${endpointName}`)
     var path, headers
     data = data || {}
     if (httpMethod === 'GET' || !httpMethod)
